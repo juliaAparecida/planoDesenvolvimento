eb02ac18e97345da74b9ab5ab874e8dccf708045
Julia Araujo <44441822+juliaAparecida@users.noreply.github.com>
2024-01-26 15:14:55 -0300

    Adicionando pastas

---
+++ Avancando com php/1241-aula06/avancando/funcoes.php
@@ -0,0 +1,12 @@
+<?php
+
+function titularComLetrasMaiusculas(array &$conta)
+{
+    $conta['titular'] = strtoupper($conta['titular']);
+}
+
+function exibeConta(array $conta)
+{
+    ['titular' => $titular, 'saldo' => $saldo] = $conta;
+    echo "<li>Titular: $titular. Saldo: $saldo</li>";
+}
---
+++ logica-de-programacao-explore-funcoes-listas/js-curso-2-aula1/js-curso-2-aula_-/desafio2.js
@@ -0,0 +1,15 @@
+let listaGenerica = [];
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+
+//Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[0]);
+
+// Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[1]);
+
+// Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[2]);
